a7feb0e241accecb367b86433352f1910bdf07ed
Олег Нурутдинов <olegnurutdinov@MacBook-Pro-Oleg.local>
2021-06-18 13:35:47 +0300

    news fix

--- resources/views/admin/news/detail.blade.php
+++ resources/views/admin/news/detail.blade.php
@@ -56,7 +56,7 @@
 
                             <div class="form-group">
                                 <label class="" for="img">Изображение</label>
-                                <img src="{{ $news->preview_img_src }}" alt="" id="img">
+                                <img src="{{Storage::disk('public')->url($news->preview_img_src) }}" alt="" id="img">
                             </div>
 
                             <div class="form-group">

a71a850a097a4ed73064d090bdc31cf0bb312a21
Олег Нурутдинов <olegnurutdinov@MacBook-Pro-Oleg.local>
2021-06-29 21:47:40 +0300

    logout

--- app/Http/Requests/App/Login/LogoutRequest.php
+++ app/Http/Requests/App/Login/LogoutRequest.php
@@ -15,4 +15,14 @@ class LogoutRequest extends FormRequest
     {
         return true;
     }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [];
+    }
 }

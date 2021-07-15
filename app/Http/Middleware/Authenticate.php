9da123d2747f8db6fc8abc9ae1849160cc6e439a
Олег Нурутдинов <olegnurutdinov@MacBook-Pro-Oleg.local>
2021-07-15 14:39:51 +0300

    logout other devices

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,8 +16,8 @@ class Authenticate extends Middleware
      */
     public function handle($request, Closure $next, ...$guards)
     {
-        dd(auth()->guard('customer'));
         if (auth()->guard('executant')->user() || auth()->guard('customer')->user()) {
+            dd('qqq');
             return $next($request);
         }
 
